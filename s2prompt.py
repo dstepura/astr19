e0f1cec95b4d517ac0b29f544a9e47b5da031db7
dstepura <dstepura@ucsc.edu>
2026-01-24 17:12:55 -0800

    Add files via upload

---
+++ s2prompt.py
@@ -0,0 +1,20 @@
+#sum of 2 floating pt. numbers
+num1 = 5.7
+num2 = 3.1
+sum_total = num1 + num2
+print("The sum is", sum_total)
+print("The data type is", type(sum_total))
+
+#difference of two integers
+num3 = 7
+num4 = 15
+diff_total = num4 - num3
+print("The difference is", diff_total)
+print("The data type is", type(diff_total))
+
+#product of a float and an integer
+num5 = 4.2
+num6 = 8
+product_total = num5 * num6
+print("The product is", product_total)
+print("The data type is", type(product_total))
